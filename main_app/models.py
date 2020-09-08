78cf24dc6a2139e5f6ce14350b17c840bc2d7e7a
partridgep <paul.partridg@gmail.com>
2020-09-07 20:46:30 -0400

    creates models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    teammates = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+
+class Bug(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    file_name = models.CharField(max_length=50)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    text = models.TextField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bug = models.ForeignKey(Bug, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+
+class Screenshot(models.Model):
+    url = models.CharField(max_length=200)
+    bug = models.ForeignKey(Bug, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Screenshot for bug_id: {self.bug_id} @{self.url}"
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Project
 
 def home(request):
   return render(request, 'home.html')
