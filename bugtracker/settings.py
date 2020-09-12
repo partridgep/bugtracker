54a4050191f5a667925eedcb17a8ddb1fde8fd6a
partridgep <paul.partridg@gmail.com>
2020-09-12 15:58:21 -0400

    prepares for deployment

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -143,3 +143,10 @@ EMAIL_HOST_USER = 'bugtracker.partridge@gmail.com'
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+# Production settings - Uncomment before deploying to Heroku
+
+'''
+'''
+import django_heroku
+django_heroku.settings(locals())
